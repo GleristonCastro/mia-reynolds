36c0704fcccc2182e878d015401798874fc4eefa
Gleriston <xpanium@gmail.com>
2023-11-01 15:09:31 -0300

    update events component

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,6 +1,20 @@
-const Events = () => {
+import SectionHeader from "../SectionHeader"
+import EventBox from "./EventBox"
+
+const getEvents = async () => {
+  const res = await fetch('http://localhost:4000/events')
+  return res.json()
+}
+
+const Events = async () => {
+  const events = await getEvents()
   return (
-    <div>Events</div>
+    <section className='section' id='tours'>
+      <div className='container mx-auto'>
+        <SectionHeader pretitle={'World Tour'} title={'Upcoming Events'} />
+        <EventBox events={events} />
+      </div>
+    </section>
   )
 }
 
