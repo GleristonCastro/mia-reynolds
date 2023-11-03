50554ba057cfcbbeb00a28354956eeaf73dad3a6
Gleriston <xpanium@gmail.com>
2023-11-02 23:17:17 -0300

    update Header component add nav and navMobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,53 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
+import NavMobile from './NavMobile'
+import Nav from './Nav'
+import MenuBtn from './MenuBtn'
+import SocialIcons from './SocialIcons'
+
 const Header = () => {
+  const [ active, setActive ] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setActive(window.scrollY > 100)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <div>Header</div>
+    <header className={`fixed z-50 w-full ${
+      active ? 'bg-[#030315] py-6 transition-all' : 'bg-transparent py-8 transition-all'
+    }`}>
+      <div className='container mx-auto flex flex-col xl:flex-row items-center justify-between'>
+        <Link
+          href={'#'}
+          className='relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0'
+        >
+          <Image src={'/assets/header/logo.svg'} fill alt='Logo' className='object-contain' />
+        </Link>
+        <Nav containerStyles='hidden xl:flex items-center gap-x-8' />
+        <NavMobile />
+        <div className='absolute right-7 top-9 z-10 xl:hidden'>
+          <MenuBtn />
+        </div>
+        <SocialIcons
+          containerStyles='flex text-[24px] gap-x-4'
+          iconStyles='hover:text-accent transition-all'
+        />
+      </div>
+    </header>
   )
 }
 
