cce64c7aa1338a3d048a1a9395716a7b0dc61dee
Gleriston <xpanium@gmail.com>
2023-11-02 21:45:31 -0300

    update page, include player component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,12 @@
-import Events from "@/components/events/Events"
+import Hero from '@/components/Hero'
+import Player from '@/components/Player'
+import Events from '@/components/events/Events'
 
 export default function Home() {
   return (
     <main>
+      <Hero />
+      <Player />
       <Events />
     </main>
   )
